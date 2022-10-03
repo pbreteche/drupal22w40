36c0f072c1e4de9dff987f0eedd385733fe9ab32
Pierre Bretéché <pbreteche@dawan.fr>
2022-10-03 13:56:17 +0200

    génération theme minimal

---
+++ web/themes/formation_theme/js/formation-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Formation behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.formationTheme = {
+    attach: function (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
