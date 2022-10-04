4c0235cc13d8d49d14eaa590575781858ec7a8c3
Pierre Bretéché <pbreteche@dawan.fr>
2022-10-04 11:50:34 +0200

    styliser le menu principal

--- web/themes/formation_theme/js/formation-theme.js
+++ web/themes/formation_theme/js/formation-theme.js
@@ -2,16 +2,30 @@
  * @file
  * Formation behaviors.
  */
-(function (Drupal) {
+(function (Drupal, $) {
 
   'use strict';
 
   Drupal.behaviors.formationTheme = {
     attach: function (context, settings) {
-
-      console.log('It works!');
-
+      once('nested-menu', '.region-primary-menu > .block-menu > .menu', context).forEach(
+        function (element) {
+          const menuItems = element.querySelectorAll('.menu-item--expanded > a');
+          for (const menuItem of menuItems) {
+            menuItem.addEventListener('click', function(event) {
+              event.preventDefault();
+              for (const otherItem of menuItems) {
+                if (otherItem === menuItem) {
+                  continue;
+                }
+                otherItem.nextElementSibling.classList.remove('opened');
+              }
+              menuItem.nextElementSibling.classList.toggle('opened');
+            })
+          }
+        }
+      )
     }
   };
 
-} (Drupal));
+} (Drupal, jQuery));
